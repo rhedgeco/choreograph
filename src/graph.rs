8433b0c7780184229d2c4e3a47be3a334fac8b05
Ryan Hedgecock <rhedgeco@gmail.com>
2025-01-16 17:02:33 -0800

    make tests clearer

--- src/graph.rs
+++ src/graph.rs
@@ -279,9 +279,15 @@ mod test {
 
     #[test]
     fn simple_branch_join() {
+        // create test inputs and calculate expected output
         const TEST_INPUT: u32 = 50;
-        const TEST_OUTPUT: u32 = ((100 + TEST_INPUT + 20) + (100 + TEST_INPUT - 10)) / 2;
+        const INITIAL_ADD: u32 = 100;
+        const ADD_AMOUNT: u32 = 20;
+        const SUB_AMOUNT: u32 = 10;
+        const TEST_OUTPUT: u32 =
+            ((TEST_INPUT + INITIAL_ADD + ADD_AMOUNT) + (TEST_INPUT + INITIAL_ADD - SUB_AMOUNT)) / 2;
 
+        // build the tasks that branch off from root then join at end
         let root_task = Graph::root(|v: u32| v + 100);
         let add_task = root_task.branch(|v| v + 20);
         let sub_task = root_task.branch(|v| v - 10);
