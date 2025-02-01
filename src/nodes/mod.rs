b5dee2972bd333067ccdfbd7ad03c27ee0d62c0b
Ryan Hedgecock <rhedgeco@gmail.com>
2025-02-01 01:48:23 -0800

    remove split node

--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -1,11 +1,9 @@
 pub mod dupe;
 pub mod future;
-pub mod split;
 pub mod sync;
 pub mod then;
 
 pub use dupe::DupeExt;
 pub use future::FutureExt;
-pub use split::SplitExt;
 pub use sync::SyncExt;
 pub use then::ThenExt;
