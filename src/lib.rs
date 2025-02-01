3851997967a4dd1d3c8c98d597b5551213e94d6b
Ryan Hedgecock <rhedgeco@gmail.com>
2025-01-31 20:19:15 -0800

    move re-export to bottom

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-// extern export self for proc macros
-pub extern crate self as choreo;
-
 pub mod graph;
 pub mod nodes;
 
 pub use graph::GraphNode;
+
+// extern export self for proc macros
+pub extern crate self as choreo;
